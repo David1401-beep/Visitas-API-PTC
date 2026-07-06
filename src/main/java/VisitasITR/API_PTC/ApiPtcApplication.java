090dc6696446d30d884f734f3e2f92894c461863
David1401-beep <20250119@ricaldone.edu.sv>
2026-07-05 21:32:17 -0600

    Iniciando API

---
+++ src/main/java/VisitasITR/API_PTC/ApiPtcApplication.java
@@ -0,0 +1,13 @@
+package VisitasITR.API_PTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPtcApplication.class, args);
+	}
+
+}
