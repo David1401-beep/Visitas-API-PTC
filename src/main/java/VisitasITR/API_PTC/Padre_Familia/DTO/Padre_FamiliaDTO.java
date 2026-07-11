19e24e6ded4a443924a57c0f78dffe02d57b2790
SamuelCabrera2025 <20240087@ricaldone.edu.sv>
2026-07-11 13:38:37 -0600

    DTO PadreFamilia

--- src/main/java/VisitasITR/API_PTC/Padre_Familia/DTO/Padre_FamiliaDTO.java
+++ src/main/java/VisitasITR/API_PTC/Padre_Familia/DTO/Padre_FamiliaDTO.java
@@ -1,4 +1,20 @@
 package VisitasITR.API_PTC.Padre_Familia.DTO;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 public class Padre_FamiliaDTO {
+
+
+    private Long id;
+
+    @NotNull(message = "ERRO1: El nombre del Padre es  obligatorio")
+    private Integer Padre_Familia;
+
+
+    @NotNull (message = "ERRO2: El telefono del padre obligatorio")
+    private  Long PAD_nombre;
 }
+
