91b08515781ff6031118a8bb3d5e9d069fda1ea0
SamuelCabrera2025 <20240087@ricaldone.edu.sv>
2026-07-11 15:05:01 -0600

    Enity,Reposity PadreFamilia

--- src/main/java/VisitasITR/API_PTC/Padre_Familia/DTO/Padre_FamiliaDTO.java
+++ src/main/java/VisitasITR/API_PTC/Padre_Familia/DTO/Padre_FamiliaDTO.java
@@ -1,20 +1,49 @@
 package VisitasITR.API_PTC.Padre_Familia.DTO;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 public class Padre_FamiliaDTO {
 
+    private Long idPadre;
 
-    private Long id;
+    @NotNull(message = "ERROR1: El nombre del padre es obligatorio")
+    private String nombre;
 
-    @NotNull(message = "ERRO1: El nombre del Padre es  obligatorio")
-    private Integer Padre_Familia;
+    @NotNull(message = "ERROR2: El apellido del padre es obligatorio")
+    private String apellido;
 
+    @NotNull(message = "ERROR3: El teléfono del padre es obligatorio")
+    private String telefono;
 
-    @NotNull (message = "ERRO2: El telefono del padre obligatorio")
-    private  Long PAD_nombre;
+    public Long getIdPadre() {
+        return idPadre;
+    }
+
+    public void setIdPadre(Long idPadre) {
+        this.idPadre = idPadre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
-
