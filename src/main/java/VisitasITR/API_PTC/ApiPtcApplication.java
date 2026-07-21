33802a3a4b67579c321a52f3644ab31642e5be1f
SamuelCabrera2025 <20240087@ricaldone.edu.sv>
2026-07-20 21:17:01 -0600

    Ultimos cambios sobre conxion base y API

--- src/main/java/VisitasITR/API_PTC/ApiPtcApplication.java
+++ src/main/java/VisitasITR/API_PTC/ApiPtcApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiPtcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPtcApplication.class, args);
 	}
-
 }
